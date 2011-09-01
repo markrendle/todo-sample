9da59bacd85cbd0d37523fd5bf3462f60bf4f9f6
markrendle <mark@markrendle.net>
2011-09-01 01:08:42 +0100

    Created solution and Migrations project

---
+++ src/ToDo/ToDo.Migrations/InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator;
+
+namespace ToDo.Migrations
+{
+    [Migration(1)]
+    public class InitialCreate : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("User")
+                .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+                .WithColumn("Email").AsString(128).NotNullable()
+                .WithColumn("EncryptedPassword").AsBinary(32).NotNullable()
+                .WithColumn("Salt").AsBinary(32).NotNullable();
+
+            Create.Table("ToDo")
+                .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+                .WithColumn("UserId").AsGuid().ForeignKey().References("FK_ToDo_User", "User", new[] {"Id"}).NotNullable()
+                .WithColumn("Text").AsString(1024).NotNullable()
+                .WithColumn("Added").AsDateTime().NotNullable()
+                .WithColumn("Done").AsDateTime().Nullable();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
