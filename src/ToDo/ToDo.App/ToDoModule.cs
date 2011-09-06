eb5de147aad4b82d62848bdc63cd459f21d051cb
markrendle <mark@markrendle.net>
2011-09-06 17:38:34 +0100

    Added functionality to todos page

--- src/ToDo/ToDo.App/ToDoModule.cs
+++ src/ToDo/ToDo.App/ToDoModule.cs
@@ -1,15 +1,33 @@
 ﻿namespace ToDo.App
 {
+    using System;
+    using System.Dynamic;
     using Nancy;
+    using Nancy.Extensions;
     using Nancy.Security;
+    using Simple.Data;
 
     public class ToDoModule : NancyModule
     {
-        public ToDoModule() : base("/todo")
+        private readonly dynamic _db = Database.OpenNamedConnection("todo");
+        public ToDoModule()
+            : base("/todo")
         {
             this.RequiresAuthentication();
 
-            Get["/"] = _ => "Secure";
+            Get["/"] = _ =>
+                           {
+                               dynamic model = new ExpandoObject();
+                               model.ToDos = _db.Users.FindAllByEmail(Context.Items["username"]).ToDos.OrderByAdded();
+                               return View["todos", model];
+                           };
+
+            Post["/"] = req =>
+                            {
+                                var user = _db.Users.FindByEmail(Context.Items["username"]);
+                                _db.Todos.Insert(UserId: user.Id, Text: Request.Form.Text);
+                                return Context.GetRedirect("~/todo/");
+                            };
         }
     }
 }
